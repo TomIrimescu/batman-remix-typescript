fee0983059e26ff507ef3584ca8dfc1860b3c26e
Tom Irimescu <tom@irimescu.com>
2023-07-06 16:11:27 -0400

    Update future flags

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  serverModuleFormat: 'cjs',
   future: {
+    v2_dev: false,
     v2_errorBoundary: true,
+    v2_headers: false,
+    v2_meta: false,
+    v2_normalizeFormMethod: false,
+    v2_routeConvention: false,
   },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
